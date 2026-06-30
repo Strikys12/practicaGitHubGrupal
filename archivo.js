6606f2343362cd143cf6f622499b104411baacbe
PaulJonaDev <jonathancaraballo.e@gmail.com>
2026-06-30 16:55:33 -0500

    Cambios realizados por Jonathan

---
+++ archivo.js
@@ -0,0 +1,4 @@
+// esto es una colaboration grupal de git 
+console.log("Hola, este es un archivo de prueba para la colaboración en grupo usando Git.");    
+let nombre = "Jonathan";
+console.log("Hola, " + nombre + "! Bienvenido a la colaboración en grupo usando Git.");
